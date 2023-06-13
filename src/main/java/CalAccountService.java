e8b0a83c97301377c7b2a79cb5ce9567f519d1b0
fei <gefei@coinflex.com>
2023-06-13 14:27:18 +0800

    init codes

---
+++ src/main/java/CalAccountService.java
@@ -0,0 +1,13 @@
+
+
+public class CalAccountService {
+
+
+    public void checkAccount(Long accountId) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
---
+++ src/main/java/dto/AccountDto.java
@@ -0,0 +1,11 @@
+package dto;
+
+
+import java.util.Date;
+
+public class AccountDto {
+    private Long id;
+    private String name;
+    private Integer age;
+    private Date time;
+}
