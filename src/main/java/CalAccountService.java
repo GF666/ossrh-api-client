f1bbfba57b5eb70c558d0184bcd2e1162fa09095
fei <gefei@coinflex.com>
2023-06-15 11:03:35 +0800

    update pom

--- src/main/java/CalAccountService.java
+++ src/main/java/CalAccountService.java
@@ -6,7 +6,10 @@ public class CalAccountService {
     public void checkAccount(Long accountId) {
 
     }
-
+    // test in stage ,update  stage test
+    // xiao yingyue xian
+    //
+    // test stage test  xian zai zhey ang deqing kuang xia ru f
     public static void main(String[] args) {
         System.out.println("args = " + args);
     }
